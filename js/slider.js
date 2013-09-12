da2405c65bc675bef2506a87f0fc55afdfbc4e2d
Stanko <stanko87@gmail.com>
2013-09-12 15:00:34 +0200

    slider

--- js/slider.js
+++ js/slider.js
@@ -3,31 +3,53 @@ $(document).ready(function(){
 		this.el = $(selector);
 		this.maxTransitionTime = 650;
 
-		this.init();
-	}
-	Slider.prototype.init = function() {
 		this.slides = this.el.find('> div');
 		this.slideCount = this.slides.length;
 		this.active = 1;
 
+		if(!this.supportTransitions()){
+			slide.maxTransitionTime = 0;
+		}
+
 		this.bindings();
-	};
+	}
 
 	Slider.prototype.bindings = function() {
 		var $this = this;
 		$('.prev').click(function(){ $this.previousSlide(); });
 		$('.next').click(function(){ $this.nextSlide(); });
+		$('.goto').click(function(){ $this.goToSlide($(this).data('index')); });
+	};
+
+
+	Slider.prototype.supportTransitions = function(first_argument) {
+		var b = document.body.style || document.documentElement.style;
+		if(typeof b['transition'] == 'string') {
+			return true;
+		}
+		return false;
 	};
 
 	Slider.prototype.goToSlide = function(slideIndex) {
+		if(slideIndex == this.active){
+			return;
+		}
+
+		if(this.el.data('animating') == 1){
+			return;
+		}
+
 		var $this = this,
 			oldIndex = this.active-1;
+
+		this.el.data('animating', 1);
 
 		this.slides.eq(oldIndex).addClass('out2');
 		this.slides.eq(slideIndex-1).addClass('out').removeClass('noTransition').removeClass('out');
 
 		setTimeout(function(){
 			$this.slides.eq(oldIndex).addClass('noTransition').addClass('out').removeClass('out2');
+			$this.el.data('animating', 0);
 		}, $this.maxTransitionTime);
 
 		this.active = slideIndex;
