65ddbf323689e3561699274bec008e63bab81b53
Stanko <stanko.tadic@gmail.com>
2015-03-25 10:20:25 +0100

    Default variables, svg icon for select, few minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,13 @@ gulp.task('default', ['sass']);
 
 gulp.task('sass', function() {
   return gulp.src('./sass/skyblue.scss')
-    .pipe(sass({ style: 'expanded' }))
+    .pipe(sass({
+        style: 'expanded',
+        errLogToConsole: false,
+        onError: function(err) {
+            return notify().write(err);
+        }
+    }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('./css/'))
     .pipe(rename({suffix: '.min'}))
